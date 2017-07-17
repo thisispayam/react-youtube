26bd2369d485d3cbbd15f0db3ef5157098de92fc
thisispayam <thisispayam@hotmail.com>
2017-07-16 23:15:49 -0400

    Adding search input and setting up a state

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import SearchBar from './components/search_bar'
+
+const API_KEY = '';
+const App = () => {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    )
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
