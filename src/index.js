219e8a29b3c51693248b6e99e0bfe41538b2c015
thisispayam <thisispayam@gmail.com>
2017-07-24 14:43:19 -0400

    added the video items and video items list clickable logic

--- src/index.js
+++ src/index.js
@@ -3,16 +3,24 @@ import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
+import VideoDetails from './components/video_details';
 
 const API_KEY = 'AIzaSyD7bfO_yBZHfsirUVq7SUMt-8ntBZNbu8s';
 class App extends Component {
   constructor (props){
     super(props);
 
-    this.state = {videos:[]};
+    this.state = {
+        videos:[],
+        selectedVideo: null
+    
+    };
 
     YTSearch({key:API_KEY, term:'surfboards'}, (videos) => {
-      this.setState({videos});
+      this.setState({
+        videos:videos,
+        selectedVideo: videos[0]
+      });
       console.log(videos);
     });
   }
@@ -20,7 +28,11 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoList videos={this.state.videos}/>
+        <VideoDetails video={this.state.selectedVideo}/>
+        <VideoList 
+            onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+            videos={this.state.videos}/>
+
       </div>
     );
   }
