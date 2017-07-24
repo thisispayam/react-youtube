aa4c0c5458b2f9270b39b3d12b9067ab98638791
thisispayam <thisispayam@gmail.com>
2017-07-24 16:46:03 -0400

    added the search functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
@@ -15,19 +16,24 @@ class App extends Component {
         selectedVideo: null
     
     };
-
-    YTSearch({key:API_KEY, term:'surfboards'}, (videos) => {
-      this.setState({
-        videos:videos,
-        selectedVideo: videos[0]
-      });
-      console.log(videos);
+      
+      this.videoSearch('surfboards');
+  }
+    videoSearch(term) {
+        
+        YTSearch({key:API_KEY, term:term}, (videos) => {
+          this.setState({
+            videos:videos,
+            selectedVideo: videos[0]
+          });
+          console.log(videos);
     });
   }
   render(){
+      const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetails video={this.state.selectedVideo}/>
         <VideoList 
             onVideoSelect={selectedVideo => this.setState({selectedVideo})}
